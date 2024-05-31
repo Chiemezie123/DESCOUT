672470d569a11a9c1a3ac1cd4f51115d5a240fe1
Nduaguba Chiemezie Anthony <anthonynduaguba123@gmail.com>
2024-05-31 18:20:15 +0100

    --fix:made root changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import "./App.css";
+
 import MainRouter from "./router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <>
+    <div>
       <MainRouter />
       <ToastContainer />
-    </>
+    </div>
   );
 }
 
